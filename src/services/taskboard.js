fa2c5df2d6f471fedd4a4e62fb58fb8c126e6c52
Roope Karhapää <roope-kar@aiven.io>
2020-11-24 10:30:15 +0200

    create service for subscribing to taskboards

--- src/services/taskboard.js
+++ src/services/taskboard.js
@@ -1,15 +1,35 @@
 export class TaskBoardService {
 
     constructor() {
-        this.subscriptions = {};
+        this.subscription;
+        this.listeners = {};
     }
 
-    subscribe(workspace, callback) {
-
+    subscribe(taskboard) {
+        return new Promise((resolve, reject) => {
+            this.subscription = new WebSocket(taskboard);
+            this.subscription.addEventListener('open', () => {
+                this.subscription.addEventListener('message', event => {
+                    this.listeners[event.data.name](event);
+                });
+                resolve();
+            });
+            this.subscription.addEventListener('error', reject);
+        });
     }
 
-    unsubscribe(workspace) {
+    unsubscribe() {
+        return new Promise((resolve) => {
+            this.subscription.addEventListener('close', () => {
+                this.listeners = {};
+                resolve();
+            });
+            this.subscription.close();
+        });
+    }
 
+    on(event, callback) {
+        this.listeners[event] = callback;
     }
 
 }
